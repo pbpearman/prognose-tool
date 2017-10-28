7f58c2344ffa9d46b8418044f13e65da5b2a8f07
waleed <waleed@4eyes.ch>
2017-10-28 10:07:40 +0200

    implemnt angular js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,31 @@
+var app = angular.module("prognose-tool", []);
+app.controller("prognoseController", function($scope) {
+    $scope.prognoseToll = {
+        PageTitle: 'Prognose-Tool',
+        categoryTitle: 'Kategorie',
+        periodTitle: 'Zeitraum',
+        categories: {
+            bergbahn: {
+                name: 'Bergbahn'
+            },
+            skischule: {
+                name: 'Skischule'
+            },
+            schwimmbad: {
+                name: 'Schwimmbad'
+            }
+        },
+        periods: {
+            oneWeek: {
+                name: '1 W.'
+            },
+            towWeeks: {
+                name: '2 W.'
+            }
+        }
+    }
+});
+
 function setActiveClass($selector) {
     $(document).ready(function(){
         $($selector).click(function() {
